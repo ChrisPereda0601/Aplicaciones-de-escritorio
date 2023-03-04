a6854700820ad3f7dbef0adac4bbdc80ce54d363
ChrisPereda0601 <cperedagonzalez@gmail.com>
2023-03-03 18:42:01 -0600

    eliminacion

--- src/rutas/index.js
+++
@@ -1,7 +0,0 @@
-//Este es mi proxy de routas
-const router = require('express').Router();
-const rutasNoticias = require('./noticias');
-
-router.use('/noticias', rutasNoticias);
-
-module.exports = router;
