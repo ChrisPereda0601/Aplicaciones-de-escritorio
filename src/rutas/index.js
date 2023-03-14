1ea32d5e8ecdde478576069f71103434609597a3
ChrisPereda0601 <cperedagonzalez@gmail.com>
2023-03-13 23:02:05 -0600

    [inicializacion] Creacion de la tarea 5

---
+++ src/rutas/index.js
@@ -0,0 +1,7 @@
+//Este es mi proxi de rutas
+const router = require('express').Router();
+const rutasTareas = require('./tareas');
+
+router.use('/tareas', rutasTareas);    //Middleware de tareas.    //Esto solo pide autenticaciOn para esto que estamos especificando.
+
+module.exports = router;
