c3f2577c58f7e0362ca7c2029967cfb4b203fc5f
Christian Alejandro Pereda <90405118+ChrisPereda0601@users.noreply.github.com>
2023-03-13 23:07:13 -0600

    Delete src directory

--- src/rutas/index.js
+++
@@ -1,7 +0,0 @@
-//Este es mi proxi de rutas
-const router = require('express').Router();
-const rutasTareas = require('./tareas');
-
-router.use('/tareas', rutasTareas);    //Middleware de tareas.    //Esto solo pide autenticaciOn para esto que estamos especificando.
-
-module.exports = router;
